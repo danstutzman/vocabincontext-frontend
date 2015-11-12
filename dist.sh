daa385adc64760d9d4ee6928fe1ffe57f38de320
Dan Stutzman <dan@danstutzman.com>
2015-11-11 17:53:23 -0700

    Compress JavaScript with UglifyJS2, using precompiled react.min.js though.

--- dist.sh
+++ dist.sh
@@ -1,21 +1,44 @@
 #!/bin/bash -ex
 cd `dirname $0`
 
-mkdir -p dist
+BROWSERIFY=node_modules/browserify/bin/cmd.js
+WATCHIFY=node_modules/watchify/bin/cmd.js
+
+rm -rf dist/*
+mkdir -p dist dist/js
+
+cat node_modules/react/dist/react.min.js > dist/js/vendor.js
+cat node_modules/react-dom/dist/react-dom.min.js >> dist/js/vendor.js
+cat >>dist/js/vendor.js <<EOF
+  function require(name) {
+    if (name === 'react') { return window.React; }
+    else if (name === 'react-dom') { return window.ReactDOM; }
+    else { throw new Error("Unknown library '" + name + "'"); }
+  }
+EOF
+#NODE_ENV=production $BROWSERIFY -r underscore -v \
+#  | node_modules/uglify-js/bin/uglifyjs --compress --mangle \
+#  >> dist/js/vendor.js
+
+$BROWSERIFY -t coffeeify src/js/app.coffee -v \
+  -x react -x react-dom -x underscore \
+  | node_modules/uglify-js/bin/uglifyjs --compress --mangle \
+  > dist/js/app.js
+
 rm -f dist/assets.json
-node_modules/hashmark/bin/hashmark build/js/*.js -l 5 -m dist/assets.json 'dist/js/{name}.{hash}{ext}'
+node_modules/hashmark/bin/hashmark dist/js/*.js -r true -l 5 -m dist/assets.json 'dist/js/{name}.{hash}{ext}'
 
 ruby -e "
 require 'json'
 assets = JSON.load(File.read('dist/assets.json'))
 assets.each do |key, value|
-  value.gsub! /^dist\//, ''
+  value.gsub! 'dist/', ''
 end
 index = File.read('src/index.html')
 index.gsub! '<script src=\'js/vendor.js\'></script>',
-  \"<script src='#{assets['build/js/vendor.js']}'></script>\"
+  \"<script src='#{assets.fetch('dist/js/vendor.js')}'></script>\"
 index.gsub! '<script src=\'js/app.js\'></script>',
-  \"<script src='#{assets['build/js/app.js']}'></script>\"
+  \"<script src='#{assets.fetch('dist/js/app.js')}'></script>\"
 File.open('dist/index.html', 'w') do |f|
   f.write index
 end
--- watch.sh
+++ watch.sh
@@ -6,6 +6,20 @@ WATCHIFY=node_modules/watchify/bin/cmd.js
 
 mkdir -p build
 mkdir -p build/js
+
 ln -sf ../src/index.html build/index.html
-$BROWSERIFY -r react -r react-dom -d -v -o build/js/vendor.js
-$WATCHIFY -t coffeeify src/js/app.coffee -d -x react -x react-dom -v -o build/js/app.js
+
+cp node_modules/react/dist/react.js build/js/vendor.js
+cat node_modules/react-dom/dist/react-dom.js >> build/js/vendor.js
+cat >>build/js/vendor.js <<EOF
+  function require(name) {
+    if (name === 'react') { return window.React; }
+    else if (name === 'react-dom') { return window.ReactDOM; }
+    else { throw new Error("Unknown library '" + name + "'"); }
+  }
+EOF
+#$BROWSERIFY -r underscore -d -v >> build/js/vendor.js
+
+$WATCHIFY -t coffeeify src/js/app.coffee -d \
+  -x react -x react-dom -x underscore \
+  -v -o build/js/app.js
